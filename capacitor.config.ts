2d87b2284144f65a3a1266e246c8744e5f7712d3
marcosborg <m.borges.mail@gmail.com>
2024-01-31 11:09:09 +0000

    Update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'catalogo360',
+  appName: 'Catálogo 360',
   webDir: 'www',
   bundledWebRuntime: false
 };
